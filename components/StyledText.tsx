e0b26bd438fcd7714894bc94b350325e23585a23
Eslam Fares Mohamed Emam <152084165+bgbos7077@users.noreply.github.com>
2024-05-11 04:34:18 +0300

    Delete components directory

--- components/StyledText.tsx
+++
@@ -1,5 +0,0 @@
-import { Text, TextProps } from './Themed';
-
-export function PoppinsText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: 'Poppins-Bold' }]} />;
-}
